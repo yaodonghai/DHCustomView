f2f9767039e18db04481cef156debde189226f05
yaodonghai <yaomac@yaodonghaideMacBook-Pro.local>
2016-08-01 22:29:03 +0800

    上传DEMO
    上传DEMO

---
+++ DHCustomViewDemo/DHCustomViewDemo/DHCustomView/DHLabel/DHLabel.h
@@ -0,0 +1,13 @@
+//
+//  DHLabel.h
+//  DHCustomViewDemo
+//
+//  Created by yaodonghai on 16/8/1.
+//  Copyright © 2016年 yaodonghai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHLabel : UILabel
+
+@end
---
+++ DHCustomViewDemo/DHCustomViewDemo/DHCustomView/DHLabel/DHLabel.m
@@ -0,0 +1,29 @@
+//
+//  DHLabel.m
+//  DHCustomViewDemo
+//
+//  Created by yaodonghai on 16/8/1.
+//  Copyright © 2016年 yaodonghai. All rights reserved.
+//
+
+#import "DHLabel.h"
+
+@implementation DHLabel
+
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self= [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
